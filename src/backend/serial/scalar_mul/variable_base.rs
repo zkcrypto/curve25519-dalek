8eaef599147b5d7063d606e9b9df5b96928a4de3
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-07-31 20:43:08 -0700

    simplify to scalar mul

--- src/backend/serial/scalar_mul/variable_base.rs
+++ src/backend/serial/scalar_mul/variable_base.rs
@@ -62,11 +62,9 @@ pub(crate) fn mul(point: &EdwardsPoint, scalar: &Scalar) -> EdwardsPoint {
     let a_bits = a_bits.iter().map(|bit| *bit == 1).collect::<Vec<bool>>();
 
     // This call to multi_scalar_multiplication does not make use of the identity point in the computation.
-    let res = AffinePoint::multi_scalar_multiplication(
+    let res = AffinePoint::scalar_multiplication(
         &a_bits,
         ed_point,
-        &[false; 256],
-        Ed25519AffinePoint::identity(),
     )
     .unwrap();
     res.into()
